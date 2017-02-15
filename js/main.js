22f1609d4aa0d6f5135d778bf3ad230b7658dbfe
Mustafa Muhammad Saad <eng.mustafa.muhammad@gmail.com>
2017-02-15 08:36:02 +0200

    Demo1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+	
+    $(document).ready(function(){
+        $(".fakeloader").fakeLoader({
+            timeToHide:73000,
+            bgColor:"#fff",
+            spinner:"spinner6"
+        });
+    });
